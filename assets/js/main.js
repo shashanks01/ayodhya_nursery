deb904c98f09fe6404b7c3704f1a46e5a6f3177e
Shashank S <shash0124@gmail.com>
2023-03-18 11:32:52 +0530

    lazy loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,20 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+function ReLoadImages(){
+    $('img[data-lazysrc]').each( function(){
+        //* set the img src from data-src
+        $( this ).attr( 'src', $( this ).attr( 'data-lazysrc' ) );
+        }
+    );
+}
+document.addEventListener('readystatechange', event => {
+  if (event.target.readyState === "interactive") {  //or at "complete" if you want it to execute in the most last state of window.
+      ReLoadImages();
+  }
+});
+
 (function() {
   "use strict";
 
